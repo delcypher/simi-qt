9c91e710d7e579ced0086841d8552b1d0deab779
Dan Liew <daniel.liew11@imperial.ac.uk>
2011-12-26 19:25:34 +0000

    * Modified hello.cxx to do a simple Qt4 example from "C++ GUI programming with Qt4" * Modified CMakeLists.txt to work with the basic components of Qt4.

--- hello.cxx
+++ hello.cxx
@@ -1,9 +1,40 @@
 #include "itkImage.h"
 #include <iostream>
-int main()
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
+
+int main(int argc, char* argv[])
 {
 	typedef itk::Image< unsigned short, 3 > ImageType;
 	ImageType::Pointer image = ImageType::New();
 	std::cout << "ITK Hello World !" << std::endl;
-	return 0;
+
+	QApplication app(argc,argv);
+	QWidget* window = new QWidget;
+	window->setWindowTitle("Simi-qt");
+
+	QSpinBox* spinBox= new QSpinBox;
+	QSlider* slider = new QSlider(Qt::Horizontal);
+	spinBox->setRange(0,130);
+	slider->setRange(0,130);
+
+	QObject::connect(spinBox,SIGNAL(valueChanged(int)),
+			slider,SLOT(setValue(int)));
+
+	QObject::connect(slider, SIGNAL(valueChanged(int)),
+			spinBox, SLOT(setValue(int)));
+
+	spinBox->setValue(35);
+
+	QHBoxLayout* layout = new QHBoxLayout;
+	layout->addWidget(spinBox);
+	layout->addWidget(slider);
+	window->setLayout(layout);
+
+	window->show();
+
+
+	return app.exec();
 }
