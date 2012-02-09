88aa28caef10e40fcc3547a3c1206aeee8d5c051
Dan Liew <daniel.liew11@imperial.ac.uk>
2012-02-09 18:05:50 +0000

    * Added Eddie's suggestions

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,11 @@
 #include <QtGui>
 
 #include "mainwindow.h"
+#include <iostream>
 
-MainWindow::MainWindow(QString& filepath) : mFilepath(filepath)
+
+
+MainWindow::MainWindow(QString& filepath) : mFilepath(filepath), currentSlice(0)
 {
 	ui = new Ui::MainWindow;
 	ui->setupUi(this); //set up user interface
@@ -10,9 +13,61 @@ MainWindow::MainWindow(QString& filepath) : mFilepath(filepath)
 	//Setup About Qt Dialog
 	connect(ui->actionAboutQt,SIGNAL(triggered()),qApp,SLOT(aboutQt()));
 
+
+	reader = vtkStructuredPointsReader::New();
+	imageView = vtkImageViewer2::New();
+
+
+
+
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_doSomething_clicked()
+{
+	reader->SetFileName(mFilepath.toStdString().c_str());
+	reader->Update();
+	imageView->SetInputConnection(reader->GetOutputPort());
+
+
+
+	double range[2];
+	reader->GetOutput()->GetScalarRange(range);
+
+       // vtkSmartPointer<vtkRenderWindowInteractor> iren =
+    //   vtkSmartPointer<vtkRenderWindowInteractor>::New();
+       // image_view->SetupInteractor( iren );
+	imageView->GetRenderer()->ResetCamera();
+
+	imageView->SetColorLevel((range[0]+range[1])/2.0); //half way point in data set
+	imageView->SetColorWindow(range[0]-range[1]); //width in dataset to use
+
+	ui->qvtkWidget->SetRenderWindow(imageView->GetRenderWindow());
+	imageView->SetSlice(currentSlice);
+	imageView->SetupInteractor(ui->qvtkWidget->GetRenderWindow()->GetInteractor());
+
+
+}
+
+
+void MainWindow::on_actionSlice_up_triggered()
+{
+	std::cerr << "hello";
+	imageView->SetSlice(++currentSlice);
+
+}
+
+
+void MainWindow::on_actionSlice_down_triggered()
+{
+	std::cerr << "hello222";
+	imageView->SetSlice(--currentSlice);
+
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,6 +5,14 @@
 #include <QString>
 #include "ui_mainwindow.h"
 
+#include "vtkImageViewer2.h"
+#include "vtkSmartPointer.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkStructuredPoints.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkStructuredPointsReader.h"
+
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
@@ -13,9 +21,21 @@ class MainWindow : public QMainWindow
 		MainWindow(QString& filepath );
 		~MainWindow();
 	
-	private:
+private slots:
+		void on_doSomething_clicked();
+		void on_actionSlice_up_triggered();
+		void on_actionSlice_down_triggered();
+
+private:
 		QString mFilepath;
 		Ui::MainWindow* ui; //handle to user interface
+		vtkSmartPointer<vtkStructuredPointsReader> reader;
+		vtkImageViewer2* imageView;
+
+		int currentSlice;
+
+
+
 };
 
 #endif
