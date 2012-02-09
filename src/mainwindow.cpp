d4e1fdabfb61b471097b919e5d69953f8d2aa8f6
Dan Liew <daniel.liew11@imperial.ac.uk>
2012-02-09 15:52:41 +0000

    Added "About Qt" dialog

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,6 +7,9 @@ MainWindow::MainWindow(QString& filepath) : mFilepath(filepath)
 	ui = new Ui::MainWindow;
 	ui->setupUi(this); //set up user interface
 
+	//Setup About Qt Dialog
+	connect(ui->actionAboutQt,SIGNAL(triggered()),qApp,SLOT(aboutQt()));
+
 }
 
 MainWindow::~MainWindow()
