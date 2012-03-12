036226d690edd2cee2702095f3b1ba64f8b8ff65
Bill Papaefstratiou <vasileios.papaefstratiou-armagos11@imperial.ac.uk>
2012-03-12 12:52:28 +0000

    Changed the layout (mainwidnow) & added a line of code in main to change the application's GUI style

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char* argv[])
 {
 	QApplication app(argc,argv);
 
+	// change the application's GUI style to "plastique"
+	QApplication::setStyle("plastique");
 
 	MainWindow* mw = new MainWindow();
 	mw->show();
