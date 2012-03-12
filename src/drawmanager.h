c86832e59313955ab8bd0214d674790d335eb4e6
Wasin Chaivaranont <lambda.hyperon@gmail.com>
2012-03-12 15:54:52 +0000

    * Add Doxygen comments to drawmanager.h

--- src/drawmanager.h
+++ src/drawmanager.h
@@ -1,3 +1,7 @@
+/*! \file drawmanager.h */
+
+//!  DrawManager.
+
 #ifndef DRAWMANAGER_H
 #define DRAWMANAGER_H
 
@@ -6,19 +10,40 @@
 #include <QSpinBox>
 #include <QComboBox>
 
+/*!
+  The class provides blocking and segmentation drawing and erasing tools.
+  Note that blocking can be either drawn one slice at a time or all slices at once.
+  Segmentation drawing and erasing can only be performed one slice at a time.
+*/
 class DrawManager : public QObject
 {
 	Q_OBJECT
 
 	public:
+		//! Class constructor.
         DrawManager(ImagePairManager* imagePairManager, QSpinBox* drawSize, QComboBox* drawType);
+
+		//! Class destructor.
 		~DrawManager();
 	
 	signals:
         void drawingDone(); //emitted to indicate drawing is done
 
 	public slots:
+		//! Drawing both blocking and segmentation function.
+			/*!
+				\param xVoxel current position of cursor in x coordinate.
+				\param yVoxel current position of cursor in y coordinate.
+				\param zVoxel current position of cursor in z coordinate.
+			*/
 		void draw(int xVoxel, int yVoxel, int zVoxel);
+
+		//! Erasing function.
+			/*!
+				\param xVoxel current position of cursor in x coordinate.
+				\param yVoxel current position of cursor in y coordinate.
+				\param zVoxel current position of cursor in z coordinate.
+			*/
 		void erase(int xVoxel, int yVoxel, int zVoxel);
 
 	private:
@@ -31,7 +56,7 @@ class DrawManager : public QObject
 		enum DrawMode
 		{
 			DRAW,
-			ERASE,
+			ERASE
 		};
 
 	
