bfa7995b5f812e4da3d41867e705c5f0b633c245
Bill Papaefstratiou <vasileios.papaefstratiou-armagos11@imperial.ac.uk>
2012-03-13 22:47:49 +0000

    Added doxygen comments for seedPointManager.h

--- src/seedpointmanager.h
+++ src/seedpointmanager.h
@@ -1,3 +1,10 @@
+/*! \file seedpointmanager.h */
+
+//!  SeedPointManager.
+/*!
+    SeedPointManager is the class responsible for managing the seed points of all slices of an image.
+*/
+
 #ifndef SEEDPOINTMANAGER_H
 #define SEEDPOINTMANAGER_H
 
@@ -12,18 +19,42 @@ class SeedPointManager : public QObject
 {
 	Q_OBJECT
 	public:
+        //! The SeedPointManager constructor.
+            /*!
+                The constructor takes as input the number of slices of an image.
+            */
 		SeedPointManager(int numberOfSlices);
-		~SeedPointManager();
+
+        //! The default SeedPointManager destructor.
+        ~SeedPointManager();
 	
-		//Gets the seed point for slice "sliceNumber". Reports (-1,-1) if no seed Point is currently set
+        //! The function gets the seed point for slice "sliceNumber". If no seed is currently set it reports (-1,-1).
+            /*!
+                \param sliceNumber is the number (integer) of the current slice.
+                \param x is the x coordinate (integer) of the seed point passed by reference.
+                \param y is the y coordinate (integer) of the seed point passed by reference.
+                \return True if the seed point is already set, false otherwise.
+            */
 		bool getSeedPoint(int sliceNumber, int& x, int& y);
 
 	signals:
+        //! Functions that signals to other classes that the seed point changed.
+            /*!
+                \param sliceNumber is the number (integer) of the current slice.
+                \param x is the x coordinate (integer) of the seed point passed by reference.
+                \param y is the y coordinate (integer) of the seed point passed by reference.
+            */
 		void seedPointChanged(int sliceNumber, int x, int y);
 
 
 	public slots:
-		//allows the seed point to manually set
+        //! Function that allows the seed point to be manually set and informs other classes.
+            /*!
+                \param x is the x coordinate (integer) of the seed point passed by reference.
+                \param y is the y coordinate (integer) of the seed point passed by reference.
+                \param sliceNumber is the number (integer) of the current slice.
+                \return Returns true when the seed point is set.
+            */
 		bool setSeedPoint(int x, int y, int sliceNumber);
 
 
