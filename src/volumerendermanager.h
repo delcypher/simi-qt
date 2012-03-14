104b2415b086751181427316083a64ca83350790
Pijika Watcharapichat <pw610@imperial.ac.uk>
2012-03-14 17:49:36 +0000

    *VolumeRenderManager documentation added

--- src/volumerendermanager.h
+++ src/volumerendermanager.h
@@ -1,5 +1,12 @@
-#ifndef RENDERMANAGER_H
-#define RENDERMANAGER_H
+//!  VolumeRenderManager.
+/*!
+  This class is responsible for 3D rendering.
+  It receives StructuredPoint data from ImagePairManager and send output to qvtk3Ddisplayer.
+  Only segmentation StructuredPoint will be rendered and displayed as 3D object.
+*/
+
+#ifndef VOLUMERENDERMANAGER_H
+#define VOLUMERENDERMANAGER_H
 
 #include <QObject>
 #include <QVTKWidget.h>
@@ -24,11 +31,27 @@ class VolumeRenderManager : public QObject
 	Q_OBJECT
 
 	public:
+	//! VolumeRenderManager constructor.
 	VolumeRenderManager(ImagePairManager* imagePairManager, QVTKWidget* qvtk3Ddisplayer);
+
+
+	//! VolumeRenderManager destructor.
 	~VolumeRenderManager();
 
 	public slots:
+
+		//! This function is to draw 3D object on qvtk3Ddisplayer (QVTKWidget) and also keep updating the drawn object.
+		    /*!
+		      This function will be called when "Render" button in mainwindow is pressed.
+		      Only segmented structuredPoint will be filtered and drawn on the widget.
+		    */
 		void render3D();
+
+		//! This function is to rotate (or flip) 3D object drawn in the QVTKWidget.
+		    /*!
+		      This function will be called when "Rotate view by 180" dropdown in mainwindow is triggered.
+		      \param flip flag checking if Rotate view by 180 is triggered
+		    */
 		void flipView(bool flip);
 
 	private:
