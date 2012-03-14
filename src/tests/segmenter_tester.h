8393d6d7780d795dd6b9186a11f72501a22073ea
Tom Brzozowski <tom.b1985@yahoo.co.uk>
2012-03-14 23:19:09 +0000

    Modified segmenter unit tests

--- src/tests/segmenter_tester.h
+++ src/tests/segmenter_tester.h
@@ -5,31 +5,42 @@
 class SegmenterTester : public QObject
 {
 	Q_OBJECT
-	/*private:
-		SeedPointManager* seedPointManager;
+        private:
+                Segmenter* segmenter;
+                SeedPointManager* seedPointManager;
+                ImagePairManager* imagePairManager;
+                QComboBox* box;
+
 
 	private slots:
 	void initTestCase()
 	{
 		//Intialise everything for test
-		seedPointManager = new SeedPointManager(50);
+                seedPointManager = new SeedPointManager(120);
+                imagePairManager = new ImagePairManager();
+                box = new QComboBox();
+
+                segmenter = new Segmenter(seedPointManager, imagePairManager, box);
 	}
 
 	void testNoSeedPoint()
 	{
-		int x=0;
-		int y=0;
+//		int x=0;
+//		int y=0;
 
-		//check we get false back
-		QVERIFY(!seedPointManager->getSeedPoint(0,x,y));
+//		//check we get false back
+//		QVERIFY(!seedPointManager->getSeedPoint(0,x,y));
 
-		QCOMPARE(x,-1);
-		QCOMPARE(y,-1);
+//		QCOMPARE(x,-1);
+//		QCOMPARE(y,-1);
 	}
 
 	void cleanupTestCase()
 	{
 		delete seedPointManager;
-	}*/
+                delete imagePairManager;
+                delete box;
+                delete segmenter;
+        }
 
 };
