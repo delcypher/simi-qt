64e866cb2986da9fcdf7c38a9d87f61288d3c3fb
Dan Liew <delcypher@gmail.com>
2012-03-15 00:00:31 +0000

    Added documentation for MainWindow

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,3 +1,4 @@
+/*! \file mainwindow.h */
 #ifndef MAIN_WINDOW_H
 #define MAIN_WINDOW_H 1
 
@@ -24,22 +25,20 @@
 //Forward Declare UiTester
 class UiTester;
 
+/*! This class provides the main user interface and creates the others classes and links them together appropriately.
+*
+*/
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
 	public:
+
+        //! Constructor
 		MainWindow();
+
+		//! Destructor
 		~MainWindow();
-
-		//helper classes
-		ImagePairManager* imagePairManager;
-		SeedPointManager* seedPointManager;
-		ViewManager* viewManager;
-		DrawManager* drawManager;
-		Segmenter* segmenter;
-		VolumeRenderManager* volumeRenderManager;
-
 
 	private slots:
 		void on_actionOpen_Image_triggered();
@@ -55,57 +54,54 @@ class MainWindow : public QMainWindow
 		void on_actionClear_Segmentation_on_All_Slices_triggered();
 		void on_actionClear_Blocking_on_All_Slices_triggered();
 
-        //toolbar slots
-        void on_actionHandTool_triggered();
-        void on_actionPenTool_triggered();
-        void on_actionCrosshairTool_triggered();
-        void on_actionEraseTool_triggered();
+		//toolbar slots
+		void on_actionHandTool_triggered();
+		void on_actionPenTool_triggered();
+		void on_actionCrosshairTool_triggered();
+		void on_actionEraseTool_triggered();
 
-        void on_minSegIntensitySlider_valueChanged(int value);
-        void on_maxSegIntensitySlider_valueChanged(int value);
+		void on_minSegIntensitySlider_valueChanged(int value);
+		void on_maxSegIntensitySlider_valueChanged(int value);
 
-        void on_doSegmentation2D_clicked();
-        void on_doSegmentation3D_clicked();
+		void on_doSegmentation2D_clicked();
+		void on_doSegmentation3D_clicked();
 
-        void on_actionClear_Drawing_triggered();
-        void on_actionClear_Segmentation_triggered();
+		void on_actionClear_Drawing_triggered();
+		void on_actionClear_Segmentation_triggered();
 
-        void on_actionLoad_Segmentation_triggered();
-	bool on_actionSave_Segmentation_triggered();
+		void on_actionLoad_Segmentation_triggered();
+		bool on_actionSave_Segmentation_triggered();
 
-        void tryEnableSegmentationWidgets();
-	void enableSegmentationWidgets();
-	void disableSegmentationWidgets();
+		void tryEnableSegmentationWidgets();
+		void enableSegmentationWidgets();
+		void disableSegmentationWidgets();
 
-	void on_do3Drender_clicked();
+		void on_do3Drender_clicked();
+		void on_doDilate2D_clicked();
+		void on_doErode2D_clicked();
+		void on_doClose2D_clicked();
+		void on_doOpen2D_clicked();
 
+		void on_actionRotate_view_by_180_toggled(bool flip);
+		void on_actionResetView_triggered();
 
-        void on_doDilate2D_clicked();
+	protected:
+        //! Handle QCloseEvents so that we can prompt the user to save if necessary
+		void closeEvent(QCloseEvent* close);
 
-        void on_doErode2D_clicked();
-
-        void on_doClose2D_clicked();
-
-        void on_doOpen2D_clicked();
-
-	void on_actionRotate_view_by_180_toggled(bool flip);
-
-	void on_actionResetView_triggered();
-
-protected:
-	  void closeEvent(QCloseEvent* close);
-
-	private:
-
-	  void on_do3Drendering_clicked();
-
-private:
+	private:		
+		//helper classes
+		ImagePairManager* imagePairManager;
+		SeedPointManager* seedPointManager;
+		ViewManager* viewManager;
+		DrawManager* drawManager;
+		Segmenter* segmenter;
+		VolumeRenderManager* volumeRenderManager;
 		QFileInfo imageInfo;
 		QDir workPath; //Directory used file open dialogs
 		Ui::MainWindow* ui; //handle to user interface
 		QActionGroup* toolbarActions;
 		QProgressDialog* progressDialog;
-
 
 
 		//setup methods
@@ -117,12 +113,13 @@ private:
 		void hideWaitDialog();
 
 
+		//Change the contrast based on what the widgets are currently set to
 		void changeContrast();
 
 		//prompt the user to save save segblock if necessary
 		bool okToContinue();
 
-		//Friend class for UI testing.
+		//! UiTester is a unit test class that needs internal access to this class
 		friend class UiTester;
 };
 
